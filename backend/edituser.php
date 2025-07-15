c76053d30d2da770ec3b77eaaa2720eb5f314882
MarkHomePro <chanon@nc.ac.th>
2025-07-15 14:05:35 +0700

    upload images

--- backend/edituser.php
+++ backend/edituser.php
@@ -28,7 +28,7 @@ include 'controls/idUser.php';
 
         <main class="p-4 flex-grow-1">
             <h2>แก้ไขผู้ใช้งาน</h2>
-            <form action="controls/editUser.php" method="POST">
+            <form action="controls/editUser.php" method="POST" enctype="multipart/form-data">
                 <input type="hidden" name="id" value="<?= $user['id']; ?>">
                 <div class="mb-3">
                     <label for="">FirstName</label>
@@ -50,6 +50,10 @@ include 'controls/idUser.php';
                     <label for="">Email</label>
                     <input type="email" name="email" class="form-control" value="<?= htmlspecialchars($user['email']); ?>">
                 </div>
+                <div class="mb-3">
+                    <label for="">Picture</label>
+                    <input type="file" name="profile_image" class="form-control">
+                </div>
                 <button type="submit" class="btn btn-primary">บันทึกการแก้ไข</button>
                 <button type="reset" class="btn btn-danger">รีเซ็ต</button>
                 <a href="user.php" class="btn btn-secondary">ย้อนกลับ</a>
@@ -59,3 +63,25 @@ include 'controls/idUser.php';
 </body>
 
 </html>
+<?php if (isset($_SESSION['success'])) : ?>
+    <script>
+        Swal.fire({
+            icon: 'success',
+            title: 'สำเร็จ',
+            text: '<?= $_SESSION['success']; ?>',
+            confirmButtonText: 'ตกลง'
+        });
+    </script>
+<?php unset($_SESSION['success']);
+endif; ?>
+<?php if (isset($_SESSION['error'])) : ?>
+    <script>
+        Swal.fire({
+            icon: 'error',
+            title: 'ผิดพลาด',
+            text: '<?= $_SESSION['error']; ?>',
+            confirmButtonText: 'ตกลง'
+        });
+    </script>
+<?php unset($_SESSION['error']);
+endif; ?>
