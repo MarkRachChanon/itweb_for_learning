59a7b5c81005090548fb07615ba3ef380659d49a
MarkHomePro <chanon@nc.ac.th>
2025-07-23 12:49:33 +0700

    database

--- backend/components/header.php
+++ backend/components/header.php
@@ -4,5 +4,6 @@
         <li class="nav-item"><a href="/itweb/backend/index.php" class="nav-link text-white">Dashboard</a></li>
         <li class="nav-item"><a href="/itweb/backend/user.php" class="nav-link text-white">จัดการผู้ใช้งาน</a></li>
         <li class="nav-item"><a href="/itweb/backend/product.php" class="nav-link text-white">จัดการสินค้า</a></li>
+        <li class="nav-item"><a href="/itweb/" class="nav-link text-white">ITWEB</a></li>
     </ul>
 </nav>
